f55cf9cea24ca7541193dc428da7bb35feafc8ab
CraudiolasBR144 <claudio.andrade@aluno.ifsp.edu.br>
2026-03-16 21:44:35 -0300

    Add files via upload

--- --Atividade 1--.sql
+++ --Atividade 1--.sql
@@ -1,33 +1,34 @@
---Atividade 1--
+-- Atividade 1
+SELECT *
+FROM student s
+INNER JOIN takes t 
+    ON s.ID = t.ID;
 
---SELECT *
-----FROM student s
---INNER JOIN takes t 
- --   ON s.ID = t.ID;
 
---Atividade 2--
---SELECT 
---    s.ID,
---    s.name,
---    COUNT(t.course_id) AS "Quantidade de cursos"
---FROM student s
---INNER JOIN takes t 
-    --ON s.ID = t.ID
---WHERE s.dept_name = 'Civil Eng.'
---GROUP BY 
+-- Atividade 2
+SELECT 
+    s.ID,
+    s.name,
+    COUNT(t.course_id) AS "Quantidade de cursos"
+FROM student s
+INNER JOIN takes t 
+    ON s.ID = t.ID
+WHERE s.dept_name = 'Civil Eng.'
+GROUP BY 
     s.ID, s.name
---ORDER BY 
-    --COUNT(t.course_id) DESC;
+ORDER BY 
+    COUNT(t.course_id) DESC;
 
---Atividade 3--
---CREATE VIEW civil_eng_students AS
---SELECT 
-    --s.ID,
-    --s.name,
-    --COUNT(t.course_id) AS quantidade_cursos
---FROM student s
---INNER JOIN takes t 
-    --ON s.ID = t.ID
---WHERE s.dept_name = 'Civil Eng.'
---GROUP BY 
-    --s.ID, s.name;
+
+-- Atividade 3
+CREATE VIEW civil_eng_students AS
+SELECT 
+    s.ID,
+    s.name,
+    COUNT(t.course_id) AS quantidade_cursos
+FROM student s
+INNER JOIN takes t 
+    ON s.ID = t.ID
+WHERE s.dept_name = 'Civil Eng.'
+GROUP BY 
+    s.ID, s.name;
